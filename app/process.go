70c358e95b3f9c74e60ca800706c70759a2581c9
levtea <rzh2000@163.com>
2022-03-14 17:32:59 +0800

    test

--- app/process.go
+++ app/process.go
@@ -1,17 +1,24 @@
 package app
 
 import (
-	"fmt"
 	"os"
 	"regexp"
 	"strings"
 
 	"github.com/gatewayorg/logbeat/share"
 	"github.com/hpcloud/tail"
+	"github.com/satyrius/gonx"
 	"go.uber.org/zap"
 )
 
-var rgx = regexp.MustCompile(`\{(.*?)\}`)
+var (
+	rgx         = regexp.MustCompile(`\{(.*?)\}`)
+	nginxFormat = `$remote_addr - $remote_user  $time_local  "$request" '
+	'$status $body_bytes_sent "$http_referer" '
+	'"$http_user_agent" "$http_x_forwarded_for"'
+	'$upstream_addr  $request_time "$upstream_response_time"'
+			  '"$upstream_cache_status" "$upstream_addr"' '$request_body`
+)
 
 type ProcessLog struct {
 	path       string
@@ -47,20 +54,28 @@ func (p *ProcessLog) TailLog(filePath string) {
 	}
 	for line := range t.Lines {
 		log.Info("Process", zap.Any("log is", line.Text))
-		resList := strings.Split(line.Text, " ")
-		if len(resList) >= 15 {
-			for i, v := range resList {
-				fmt.Printf("log index is %d, log is %s \n", i, v)
-			}
-			log.Info("Process", zap.Any("log remoteAddr is", resList[0]))
-			log.Info("Process", zap.Any("log remoteUser is", resList[3]))
-			log.Info("Process", zap.Any("log timeLocal is", resList[4]+resList[5]))
-			log.Info("Process", zap.Any("log request is", resList[7]+" "+resList[8]+" "+resList[9]))
-			log.Info("Process", zap.Any("log status is", resList[10]))
-		}
-		log.Info("Process", zap.Any("log request body is", rgx.FindStringSubmatch(line.Text)))
+		// resList := strings.Split(line.Text, " ")
+		// if len(resList) >= 15 {
+		// 	for i, v := range resList {
+		// 		fmt.Printf("log index is %d, log is %s \n", i, v)
+		// 	}
+		// 	log.Info("Process", zap.Any("log remoteAddr is", resList[0]))
+		// 	log.Info("Process", zap.Any("log remoteUser is", resList[3]))
+		// 	log.Info("Process", zap.Any("log timeLocal is", resList[4]+resList[5]))
+		// 	log.Info("Process", zap.Any("log request is", resList[7]+" "+resList[8]+" "+resList[9]))
+		// 	log.Info("Process", zap.Any("log status is", resList[10]))
+		// }
+		// responseList := rgx.FindStringSubmatch(line.Text)
+		// if len(responseList) == 1 {
+		// 	log.Info("Process", zap.Any("log request body is", responseList[0]))
+		// }
+
+		reader := gonx.NewReader(strings.NewReader(line.Text), nginxFormat)
+		res, _ := reader.Read()
+		log.Info("Process is", zap.Any("res", res))
 
 	}
+
 }
 
 func StartProcess(dir string) {
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/Ankr-network/kit v1.8.7
 	github.com/hpcloud/tail v1.0.0
+	github.com/satyrius/gonx v1.3.0
 	github.com/urfave/cli/v2 v2.3.0
 	go.uber.org/zap v1.15.0
 )
