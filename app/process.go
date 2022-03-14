1d93f05914f729c509c982f200e1b7a7caef4479
levtea <rzh2000@163.com>
2022-03-14 16:33:18 +0800

    log

--- app/process.go
+++ app/process.go
@@ -3,12 +3,15 @@ package app
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/gatewayorg/logbeat/share"
 	"github.com/hpcloud/tail"
 	"go.uber.org/zap"
 )
+
+var rgx = regexp.MustCompile(`\{(.*?)\}`)
 
 type ProcessLog struct {
 	path       string
@@ -45,17 +48,18 @@ func (p *ProcessLog) TailLog(filePath string) {
 	for line := range t.Lines {
 		log.Info("Process", zap.Any("log is", line.Text))
 		resList := strings.Split(line.Text, " ")
-		for i, v := range resList {
-			fmt.Printf("log index is %d, log is %s \n", i, v)
+		if len(resList) >= 15 {
+			for i, v := range resList {
+				fmt.Printf("log index is %d, log is %s \n", i, v)
+			}
+			log.Info("Process", zap.Any("log remoteAddr is", resList[0]))
+			log.Info("Process", zap.Any("log remoteUser is", resList[3]))
+			log.Info("Process", zap.Any("log timeLocal is", resList[4]+resList[5]))
+			log.Info("Process", zap.Any("log request is", resList[7]+" "+resList[8]+" "+resList[9]))
+			log.Info("Process", zap.Any("log status is", resList[10]))
 		}
-		log.Info("Process", zap.Any("log remoteAddr is", resList[0]))
-		log.Info("Process", zap.Any("log remoteUser is", resList[3]))
-		log.Info("Process", zap.Any("log timeLocal is", resList[4]+resList[5]))
-		log.Info("Process", zap.Any("log request is", resList[7]+" "+resList[8]+" "+resList[9]))
-		log.Info("Process", zap.Any("log status is", resList[10]))
-		log.Info("Process", zap.Any("log bodyBytesSent is", resList[11]))
-		log.Info("Process", zap.Any("log requestTime is", resList[16]))
-		log.Info("Process", zap.Any("log requestBody is", resList[18]))
+		log.Info("Process", zap.Any("log request body is", rgx.FindStringSubmatch(line.Text)))
+
 	}
 }
 
